c367ff9efb67b60560c0d205019669e7d2f2c8f0
Simon PARIS <ps205947@etu.unice.fr>
2016-04-06 09:51:13 +0200

    again

--- tcf/business/src/main/java/entities/Customer.java
+++ tcf/business/src/main/java/entities/Customer.java
@@ -15,7 +15,7 @@ public class Customer implements Serializable {
 
 	public Customer() {}
 	public Customer(String n, String c) {
-		System.out.println("en moi");
+		System.out.println("encore moi");
 		this.name = n; this.creditCard = c; }
 
 	public String getName() {
