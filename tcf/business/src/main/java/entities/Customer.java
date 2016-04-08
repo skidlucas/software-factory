f7e895cca2ceaecd5c9daf9947635e94c696f728
Simon PARIS <ps205947@etu.unice.fr>
2016-04-08 08:12:38 +0200

    va faire tout noir

--- tcf/business/src/main/java/entities/Customer.java
+++ tcf/business/src/main/java/entities/Customer.java
@@ -15,7 +15,7 @@ public class Customer implements Serializable {
 
 	public Customer() {}
 	public Customer(String n, String c) {
-		System.out.println("il va faire tout noir");
+		System.out.println("il va faire tout noir!");
 		this.name = n; this.creditCard = c; }
 
 	public String getName() {
